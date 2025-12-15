edddb08c5280b54586564460da91564a5a3a7caf
Abubakar Zubair Idris <abubakarzubair@summitbankng.com>
2025-12-15 11:44:05 +0100

    Branches and atms added

--- src/app/atms/page.tsx
+++ src/app/atms/page.tsx
@@ -103,7 +103,7 @@ export default function Atms() {
                   </div>
                 ))}
               </div>
-            </div>
+            </div> 
           ))}
         </div>
       </section>
